7c2b6803a6236746198b02a339b9a1d1003a114d
Guilherme Martins <academicgmag@gmail.com>
2023-07-03 20:22:26 -0300

    add source

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      name: "Issues",
+      source_url: "https://github.com/guimartiins/issues"
       deps: deps()
     ]
   end
